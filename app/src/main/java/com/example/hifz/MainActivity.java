4979034c79585573d66f5d9b6dcedb8f8219f030
faqih <a.faqih@students.esqbs.ac.id>
2021-10-15 23:17:26 +0700

    add new page

--- app/src/main/java/com/example/hifz/MainActivity.java
+++ app/src/main/java/com/example/hifz/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.hifz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Cek first
+    }
+
+    public void intent (View view){
+        Intent i = new Intent(MainActivity.this, login.class);
+        startActivity(i);
     }
 }
