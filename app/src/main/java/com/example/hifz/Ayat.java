7741f97872c02036b537b80b099a32ea79909ec3
afifah21 <khonsamutmainnah@gmail.com>
2022-01-01 15:39:45 +0700

    halaman ayat

--- app/src/main/java/com/example/hifz/Ayat.java
+++ app/src/main/java/com/example/hifz/Ayat.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ziyadahFour extends AppCompatActivity {
+public class ayat extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ziyadah_four);
+        setContentView(R.layout.activity_ayat2);
     }
 }
--- app/src/main/java/com/example/hifz/surat.java
+++ app/src/main/java/com/example/hifz/surat.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ziyadahThree extends AppCompatActivity {
+public class surat extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ziyadah_three);
+        setContentView(R.layout.activity_surat);
     }
 }
